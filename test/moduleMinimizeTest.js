bf6550ac5a651f64b9e0d71e3c9f8bdbc367bffd
Tobias Koppers <tobias.koppers@googlemail.com>
2015-09-02 15:50:49 +0200

    fixed test for deprecated options

--- test/moduleMinimizeTest.js
+++ test/moduleMinimizeTest.js
@@ -12,6 +12,6 @@ describe("module", function() {
 		var source = fs.readFileSync(path.join(testCasesPath, name, "source.css"), "utf-8");
 		var expected = fs.readFileSync(path.join(testCasesPath, name, "expected.css"), "utf-8");
 
-		test(name, source, expected, "?module&minimize&sourceMap&-comments&localIdentName=_[local]_");
+		test(name, source, expected, "?module&minimize&sourceMap&-discardComments&localIdentName=_[local]_");
 	});
 });
