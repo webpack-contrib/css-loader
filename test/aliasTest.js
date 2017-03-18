c5fbbdea38e1d5d0bbd9d0fbca50006cde6e22d5
Theo <bbtfrr@gmail.com>
2017-03-18 11:43:20 +0800

    refactor resolve function

--- test/aliasTest.js
+++ test/aliasTest.js
@@ -3,28 +3,28 @@
 var test = require("./helpers").test;
 
 describe("alias", function() {
-  var css = ".className { background: url(./path/to/file.png); }";
-  var exports = {
-    without: [
-      [1, ".className { background: url({./path/to/file.png}); }", ""]
-    ],
-    onlyModule: [
-      [1, ".className { background: url({module/file.png}); }", ""]
-    ],
-    exactMatch: [
-      [1, ".className { background: url({module/file.png}); }", ""]
-    ],
-    notExactMatch: [
-      [1, ".className { background: url({./path/to/file.png}); }", ""]
-    ]
-  };
+	var css = ".className { background: url(./path/to/file.png); }";
+	var exports = {
+		without: [
+			[1, ".className { background: url({./path/to/file.png}); }", ""]
+		],
+		onlyModule: [
+			[1, ".className { background: url({module/file.png}); }", ""]
+		],
+		exactMatch: [
+			[1, ".className { background: url({module/file.png}); }", ""]
+		],
+		notExactMatch: [
+			[1, ".className { background: url({./path/to/file.png}); }", ""]
+		]
+	};
 
-  function aliasOptions(alias) {
-    return { options: { context: "", cssLoader: { alias: alias }}}
-  }
+	function aliasOptions(alias) {
+		return { query: { alias: alias }}
+	}
 
-  test("without", css, exports.without);
-  test("onlyModule", css, exports.onlyModule, aliasOptions({ "./path/to": "module" }));
-  test("exactMatch", css, exports.exactMatch, aliasOptions({ "./path/to/file.png$": "module/file.png" }));
-  test("notExactMatch", css, exports.notExactMatch, aliasOptions({ "./path/to/file.jpg$": "module/file.jpg" }));
+	test("without", css, exports.without);
+	test("onlyModule", css, exports.onlyModule, aliasOptions({ "./path/to": "module" }));
+	test("exactMatch", css, exports.exactMatch, aliasOptions({ "./path/to/file.png$": "module/file.png" }));
+	test("notExactMatch", css, exports.notExactMatch, aliasOptions({ "./path/to/file.jpg$": "module/file.jpg" }));
 });
