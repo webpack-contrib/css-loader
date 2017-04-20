956bad7980fbe8455eafd4c88b52640e4f0983ce
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2017-04-20 20:09:39 +0300

    fix: imported variables are replaced in exports if followed by a comma (#504)

--- test/valuesTest.js
+++ test/valuesTest.js
@@ -66,4 +66,55 @@ describe("values", function() {
 			})()
 		}
 	);
+	testLocal("should import values contain comma",
+		"@value color from './file1'; @value shadow: 0 0 color,0 0 color; .ghi { box-shadow: shadow; }", [
+			[ 2, "", "" ],
+			[ 1, ".ghi { box-shadow: 0 0 red,0 0 red; }", "" ]
+		], {
+			color: "red",
+			shadow: "0 0 red,0 0 red"
+		}, "", {
+			"./file1": (function() {
+				var a =  [[2, "", ""]];
+				a.locals = {
+					color: "red",
+				};
+				return a;
+			})()
+		}
+	);
+	testLocal("should import values contain comma and space before comma",
+		"@value color from './file1'; @value shadow: 0 0 color ,0 0 color; .ghi { box-shadow: shadow; }", [
+			[ 2, "", "" ],
+			[ 1, ".ghi { box-shadow: 0 0 red ,0 0 red; }", "" ]
+		], {
+			color: "red",
+			shadow: "0 0 red ,0 0 red"
+		}, "", {
+			"./file1": (function() {
+				var a =  [[2, "", ""]];
+				a.locals = {
+					color: "red",
+				};
+				return a;
+			})()
+		}
+	);
+	testLocal("should import values contain tralling comma and space after comma",
+		"@value color from './file1'; @value shadow: 0 0 color, 0 0 color; .ghi { box-shadow: shadow; }", [
+			[ 2, "", "" ],
+			[ 1, ".ghi { box-shadow: 0 0 red, 0 0 red; }", "" ]
+		], {
+			color: "red",
+			shadow: "0 0 red, 0 0 red"
+		}, "", {
+			"./file1": (function() {
+				var a =  [[2, "", ""]];
+				a.locals = {
+					color: "red",
+				};
+				return a;
+			})()
+		}
+	);
 });
