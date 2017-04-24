e2befc32e32a0b6bf96d5a62486bcc906725b216
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-04-25 00:59:46 +0200

    tests for higher coverage.

--- test/externalsTest.js
+++ test/externalsTest.js
@@ -15,50 +15,53 @@ describe("externals", function() {
 		};
 	});
 
-	testLocal(
-		// name
-		"composes class from external module",
-
-		// input
-		[
-			":local(.c1) { composes: c2 from \"external\"; b: 1; }",
-			":local(.c3) { composes: c1; b: 3; }",
-			":local(.c5) { composes: c2 c4 from \"external\"; b: 5; }"
-		].join("\n"),
-
-		// result
-		[
-			[
-				1,
-				[
-					"._c1 { b: 1; }",
-					"._c3 { b: 3; }",
-					"._c5 { b: 5; }"
-				].join("\n"), ""
-			]
-		],
-
-		// localsResult
+	[
 		{
-			c1: "_c1 imported-c2",
-			c3: "_c3 _c1 imported-c2",
-			c5: "_c5 imported-c2 imported-c4"
+			name: "composes class from external module, with query string",
+			query: '?localIdentName=_[local]&externals={"external":"External"}',
 		},
+		{
+			name: "composes class from external module, with externals as an array",
+			query: '?{"localIdentName":"_[local]","externals":[{"external":"External"}]}',
+		}
+	].forEach(function (t) {
+		testLocal(
+			// name
+			t.name,
 
-		// query
-		'?localIdentName=_[local]&externals={"external":"External"}',
+			// input
+			[
+				":local(.c1) { composes: c2 from \"external\"; b: 1; }",
+				":local(.c3) { composes: c1; b: 3; }",
+				":local(.c5) { composes: c2 c4 from \"external\"; b: 5; }"
+			].join("\n"),
 
-		// @TODO: test with query as an object too
-		// {
-		// 	localIdentName: '_[local]',
-		// 	externals: {
-		// 		external: 'External'
-		// 	}
-		// },
+			// result
+			[
+				[
+					1,
+					[
+						"._c1 { b: 1; }",
+						"._c3 { b: 3; }",
+						"._c5 { b: 5; }"
+					].join("\n"), ""
+				]
+			],
 
-		// modules
-		{}
-	);
+			// localsResult
+			{
+				c1: "_c1 imported-c2",
+				c3: "_c3 _c1 imported-c2",
+				c5: "_c5 imported-c2 imported-c4"
+			},
+
+			// query
+			t.query,
+
+			// modules
+			{}
+		);
+	});
 
 	after(function () {
 		delete global.External;
