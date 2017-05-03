a228120d770a8f33bf3779bc358980ae1f9cf04c
Tobias Koppers <tobias.koppers@googlemail.com>
2017-05-03 15:26:18 +0200

    run tests combined with cssnano + source maps

--- src/parse-at-import.js
+++ src/parse-at-import.js
@@ -61,6 +61,7 @@ export default {
     {
       '"([^\\\\"]|\\\\.)*"': atImportUrlMatch,
       '\'([^\\\\\']|\\\\.)*\'': atImportUrlMatch,
+      '[^\\)]+': atImportUrlMatch,
     },
     'nothingElse',
   ],
--- test/webpack.test.js
+++ test/webpack.test.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import webpack from 'webpack';
+import cssnano from 'cssnano';
 
 function mkdir(p) {
   try {
@@ -10,57 +11,105 @@ function mkdir(p) {
   }
 }
 
+const OPTIONS = {
+  normal: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: {
+          loader: path.resolve(__dirname, '../src'),
+          options: {
+            sourceMap: true,
+            sourceMapContext: __dirname,
+          },
+        },
+      },
+    ],
+  },
+  otherConfig: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: {
+          loader: path.resolve(__dirname, '../src'),
+          options: {
+            sourceMap: {
+              columns: true,
+            },
+            sourceMapPrefix: 'css:///',
+            sourceMapContext: path.resolve(__dirname, '../src'),
+          },
+        },
+      },
+    ],
+  },
+  cssnano: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: path.resolve(__dirname, '../src'),
+            options: {
+              sourceMap: true,
+              sourceMapContext: false,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                cssnano(),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
 describe('webpack', () => {
   const fixtures = path.resolve(__dirname, 'webpack-fixtures');
   const output = path.resolve(__dirname, '.output');
   const tests = fs.readdirSync(fixtures);
   tests.forEach((filename) => {
-    it(`should process ${filename}`, () => new Promise((resolve, reject) => {
-      const outputDirectory = path.resolve(output, filename.replace(/[^a-zA-Z0-9]+/g, '_'));
-      mkdir(outputDirectory);
-      webpack({
-        entry: `./${filename}/index.css`,
-        context: fixtures,
-        output: {
-          path: outputDirectory,
-          filename: 'bundle.js',
-          libraryTarget: 'commonjs2',
-        },
-        module: {
-          rules: [
-            {
-              test: /\.css$/,
-              use: {
-                loader: path.resolve(__dirname, '../src'),
-                options: {
-                  sourceMap: true,
-                  sourceMapContext: __dirname,
-                },
-              },
-            },
-          ],
-        },
-      }, (err, stats) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        try {
-          expect(stats.toString('errors-only')).toMatchSnapshot();
-          const result = require(path.resolve(outputDirectory, 'bundle.js')); // eslint-disable-line
-          expect(result).toMatchSnapshot();
-          expect(result.default.toString()).toMatchSnapshot();
-          for (const item of result.default) {
-            fs.writeFileSync(path.resolve(outputDirectory, `result${item[0]}.css`), item[1], 'utf-8');
-            item[3].file = `result${item[0]}.css`;
-            fs.writeFileSync(path.resolve(outputDirectory, `result${item[0]}.css.map`), JSON.stringify(item[3]), 'utf-8');
+    Object.keys(OPTIONS).forEach((optionsName) => {
+      const options = OPTIONS[optionsName];
+      it(`should process ${filename} with ${optionsName} options`, () => new Promise((resolve, reject) => {
+        const outputDirectory = path.resolve(output, optionsName, filename.replace(/[^a-zA-Z0-9]+/g, '_'));
+        mkdir(outputDirectory);
+        webpack({
+          entry: `./${filename}/index.css`,
+          context: fixtures,
+          output: {
+            path: outputDirectory,
+            filename: 'bundle.js',
+            libraryTarget: 'commonjs2',
+          },
+          module: options,
+        }, (err, stats) => {
+          if (err) {
+            reject(err);
+            return;
           }
-        } catch (e) {
-          reject(e);
-          return;
-        }
-        resolve();
-      });
-    }));
+          try {
+            expect(stats.toString('errors-only')).toMatchSnapshot();
+            const result = require(path.resolve(outputDirectory, 'bundle.js')); // eslint-disable-line
+            expect(result).toMatchSnapshot();
+            expect(result.default.toString()).toMatchSnapshot();
+            for (const item of result.default) {
+              fs.writeFileSync(path.resolve(outputDirectory, `result${item[0]}.css`), item[1], 'utf-8');
+              item[3].file = `result${item[0]}.css`;
+              fs.writeFileSync(path.resolve(outputDirectory, `result${item[0]}.css.map`), JSON.stringify(item[3]), 'utf-8');
+            }
+          } catch (e) {
+            reject(e);
+            return;
+          }
+          resolve();
+        });
+      }), 20000);
+    });
   });
 });
