5c6d8b050d3068d0d7b6cd3a4578433a208791e6
Joseph Bushell <joseph.bushell@oddschecker.com>
2017-06-13 16:44:48 +0100

    added tests

--- test/camelCaseTest.js
+++ test/camelCaseTest.js
@@ -5,6 +5,7 @@ var testRaw = require("./helpers").testRaw;
 
 describe("camelCase", function() {
 	var css = ".btn-info_is-disabled { color: blue; }";
+	var cssMultipleDashes = ".btn--info_is-disabled { color: blue; }";
 	var mixedCss = ".btn-info_is-disabled { color: blue; } .simple { color: red; }";
 	var exports = {
 		with: [
@@ -16,6 +17,9 @@ describe("camelCase", function() {
 		dashes: [
 			[1, "._1L-rnCOXCE_7H94L5XT4uB { color: blue; }", ""]
 		],
+		multipleDashes: [
+			[1, "._3JUlsKrl__OF70Fq391jEw { color: blue; }", ""]
+		],
 		withoutOnly: [
 			[1, "._1L-rnCOXCE_7H94L5XT4uB { color: blue; } .KKtodWG-IuEaequFjAsoJ { color: red; }", ""]
 		],
@@ -26,11 +30,13 @@ describe("camelCase", function() {
 	exports.with.locals = {'btn-info_is-disabled': '_1L-rnCOXCE_7H94L5XT4uB'};
 	exports.without.locals = {btnInfoIsDisabled: '_1L-rnCOXCE_7H94L5XT4uB', 'btn-info_is-disabled': '_1L-rnCOXCE_7H94L5XT4uB'};
 	exports.dashes.locals = {btnInfo_isDisabled: '_1L-rnCOXCE_7H94L5XT4uB', 'btn-info_is-disabled': '_1L-rnCOXCE_7H94L5XT4uB'};
+	exports.multipleDashes.locals = {btnInfo_isDisabled: '_3JUlsKrl__OF70Fq391jEw', 'btn--info_is-disabled': '_3JUlsKrl__OF70Fq391jEw'};
 	exports.withoutOnly.locals = {btnInfoIsDisabled: '_1L-rnCOXCE_7H94L5XT4uB', simple: 'KKtodWG-IuEaequFjAsoJ'};
 	exports.dashesOnly.locals = {btnInfo_isDisabled: '_1L-rnCOXCE_7H94L5XT4uB', simple: 'KKtodWG-IuEaequFjAsoJ'};
 	test("with", css, exports.with, "?modules");
 	test("without", css, exports.without, "?modules&camelCase");
 	test("dashes", css, exports.dashes, "?modules&camelCase=dashes");
+	test("multipleDashes", cssMultipleDashes, exports.multipleDashes, "?modules&camelCase=dashes");
 	// Remove this option in v1.0.0 and make the removal of the original classname the default behaviour. See #440.
 	test("withoutOnly", mixedCss, exports.withoutOnly, "?modules&camelCase=only");
 	// Remove this option in v1.0.0 and make the removal of the original classname the default behaviour. See #440.
