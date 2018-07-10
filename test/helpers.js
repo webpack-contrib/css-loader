d22c14f228cbf1a3907bc9db10f270bcf04bda9a
evilebottnawi <sheo13666q@gmail.com>
2018-07-10 17:09:51 +0300

    refactor: escape

--- test/helpers.js
+++ test/helpers.js
@@ -11,8 +11,8 @@ function getEvaluated(output, modules) {
 		fn(m, m.exports, function(module) {
 			if(module.indexOf("runtime") >= 0)
 				return require("../lib/runtime");
-			if(module.indexOf("url/escape") >= 0)
-				return require("../lib/url/escape");
+			if(module.indexOf("escape") >= 0)
+				return require("../lib/escape");
 			if(module.indexOf("-!/path/css-loader!") === 0)
 				module = module.substr(19);
 			if(modules && module in modules)
