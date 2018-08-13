027c8ca210f05a7261ba104e5bb2bc6e0e02d034
Nick Petruzzelli <code.npetruzzelli@gmail.com>
2018-08-13 18:19:38 -0400

    Update assertions used in tests for source maps.
    Signed-off-by: Nick Petruzzelli <code.npetruzzelli@gmail.com>

--- test/sourceMapTest.js
+++ test/sourceMapTest.js
@@ -44,7 +44,6 @@ describe("source maps", function() {
 				file: 'test.css',
 				mappings: 'AAAA,SAAS,SAAS,EAAE',
 				names: [],
-				sourceRoot: '',
 				sources: [ '/folder/test.css' ],
 				sourcesContent: [ '.class { a: b c d; }' ],
 				version: 3
@@ -70,7 +69,6 @@ describe("source maps", function() {
                 file: 'test.css',
                 mappings: 'AAAA,SAAS,SAAS,EAAE',
                 names: [],
-                sourceRoot: '',
                 sources: [ '/folder/test.css' ],
                 sourcesContent: [ '.class { a: b c d; }' ],
                 version: 3
@@ -83,6 +81,7 @@ describe("source maps", function() {
 	testMap("generate sourceMap (1 loader)", ".class { a: b c d; }", undefined, {
 		loaders: [{request: "/path/css-loader"}],
 		resource: "/folder/test.css",
+		resourcePath: "/folder/test.css",
 		request: "/path/css-loader!/folder/test.css",
 		query: "?sourceMap"
 	}, [
@@ -90,8 +89,7 @@ describe("source maps", function() {
 			file: 'test.css',
 			mappings: 'AAAA,SAAS,SAAS,EAAE',
 			names: [],
-			sourceRoot: '',
-			sources: [ '/folder/test.css' ],
+			sources: [ 'test.css' ],
 			sourcesContent: [ '.class { a: b c d; }' ],
 			version: 3
 		}]
@@ -99,6 +97,7 @@ describe("source maps", function() {
 	testMap("generate sourceMap (1 loader, relative)", ".class { a: b c d; }", undefined, {
 		loaders: [{request: "/path/css-loader"}],
 		resource: "/folder/test.css",
+		resourcePath: "/folder/test.css",
 		request: "/path/css-loader!/folder/test.css",
 		query: "?sourceMap"
 	}, [
@@ -106,8 +105,7 @@ describe("source maps", function() {
 			file: 'test.css',
 			mappings: 'AAAA,SAAS,SAAS,EAAE',
 			names: [],
-			sourceRoot: '',
-			sources: [ '/folder/test.css' ],
+			sources: [ 'test.css' ],
 			sourcesContent: [ '.class { a: b c d; }' ],
 			version: 3
 		}]
@@ -115,6 +113,7 @@ describe("source maps", function() {
 	testMap("generate sourceMap (1 loader, data url)", ".class { background-image: url(\"data:image/svg+xml;charset=utf-8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 42 26' fill='%23007aff'><rect width='4' height='4'/><rect x='8' y='1' width='34' height='2'/><rect y='11' width='4' height='4'/><rect x='8' y='12' width='34' height='2'/><rect y='22' width='4' height='4'/><rect x='8' y='23' width='34' height='2'/></svg>\"); }", undefined, {
 		loaders: [{request: "/path/css-loader"}],
 		resource: "/folder/test.css",
+		resourcePath: "/folder/test.css",
 		request: "/path/css-loader!/folder/test.css",
 		query: "?sourceMap"
 	}, [
@@ -122,8 +121,7 @@ describe("source maps", function() {
 			file: 'test.css',
 			mappings: 'AAAA,SAAS,6WAA6W,EAAE',
 			names: [],
-			sourceRoot: '',
-			sources: [ '/folder/test.css' ],
+			sources: [ 'test.css' ],
 			sourcesContent: [ '.class { background-image: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 42 26\' fill=\'%23007aff\'><rect width=\'4\' height=\'4\'/><rect x=\'8\' y=\'1\' width=\'34\' height=\'2\'/><rect y=\'11\' width=\'4\' height=\'4\'/><rect x=\'8\' y=\'12\' width=\'34\' height=\'2\'/><rect y=\'22\' width=\'4\' height=\'4\'/><rect x=\'8\' y=\'23\' width=\'34\' height=\'2\'/></svg>"); }' ],
 			version: 3
 		}]
@@ -131,6 +129,7 @@ describe("source maps", function() {
 	testMap("generate sourceMap (1 loader, encoded data url)", ".class { background-image: url(\"data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%2042%2026%27%20fill%3D%27%23007aff%27%3E%3Crect%20width%3D%274%27%20height%3D%274%27%2F%3E%3Crect%20x%3D%278%27%20y%3D%271%27%20width%3D%2734%27%20height%3D%272%27%2F%3E%3Crect%20y%3D%2711%27%20width%3D%274%27%20height%3D%274%27%2F%3E%3Crect%20x%3D%278%27%20y%3D%2712%27%20width%3D%2734%27%20height%3D%272%27%2F%3E%3Crect%20y%3D%2722%27%20width%3D%274%27%20height%3D%274%27%2F%3E%3Crect%20x%3D%278%27%20y%3D%2723%27%20width%3D%2734%27%20height%3D%272%27%2F%3E%3C%2Fsvg%3E\"); }", undefined, {
 		loaders: [{request: "/path/css-loader"}],
 		resource: "/folder/test.css",
+		resourcePath: "/folder/test.css",
 		request: "/path/css-loader!/folder/test.css",
 		query: "?sourceMap"
 	}, [
@@ -138,8 +137,7 @@ describe("source maps", function() {
 			file: 'test.css',
 			mappings: 'AAAA,SAAS,mmBAAmmB,EAAE',
 			names: [],
-			sourceRoot: '',
-			sources: [ '/folder/test.css' ],
+			sources: [ 'test.css' ],
 			sourcesContent: [ '.class { background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%2042%2026%27%20fill%3D%27%23007aff%27%3E%3Crect%20width%3D%274%27%20height%3D%274%27%2F%3E%3Crect%20x%3D%278%27%20y%3D%271%27%20width%3D%2734%27%20height%3D%272%27%2F%3E%3Crect%20y%3D%2711%27%20width%3D%274%27%20height%3D%274%27%2F%3E%3Crect%20x%3D%278%27%20y%3D%2712%27%20width%3D%2734%27%20height%3D%272%27%2F%3E%3Crect%20y%3D%2722%27%20width%3D%274%27%20height%3D%274%27%2F%3E%3Crect%20x%3D%278%27%20y%3D%2723%27%20width%3D%2734%27%20height%3D%272%27%2F%3E%3C%2Fsvg%3E"); }' ],
 			version: 3
 		}]
@@ -147,6 +145,7 @@ describe("source maps", function() {
 	testMap("generate sourceMap (2 loaders)", ".class { a: b c d; }", undefined, {
 		loaders: [{request: "/path/css-loader"}, {request: "/path/sass-loader"}],
 		resource: "/folder/test.scss",
+		resourcePath: "/folder/test.scss",
 		request: "/path/css-loader!/path/sass-loader!/folder/test.scss",
 		query: "?sourceMap"
 	}, [
@@ -154,8 +153,7 @@ describe("source maps", function() {
 			file: 'test.scss',
 			mappings: 'AAAA,SAAS,SAAS,EAAE',
 			names: [],
-			sourceRoot: '',
-			sources: [ '/folder/test.scss' ],
+			sources: [ 'test.scss' ],
 			sourcesContent: [ '.class { a: b c d; }' ],
 			version: 3
 		}]
@@ -165,12 +163,13 @@ describe("source maps", function() {
 		mappings: 'AAAA,SAAS,SAAS,EAAE',
 		names: [],
 		sourceRoot: '',
-		sources: [ '/folder/test.scss' ],
+		sources: [ 'test.scss' ],
 		sourcesContent: [ '.class { a: b c d; }' ],
 		version: 3
 	}, {
 		loaders: [{request: "/path/css-loader"}, {request: "/path/sass-loader"}],
 		resource: "/folder/test.scss",
+		resourcePath: "/folder/test.scss",
 		request: "/path/css-loader!/path/sass-loader!/folder/test.scss",
 		query: "?sourceMap"
 	}, [
@@ -178,8 +177,7 @@ describe("source maps", function() {
 			file: 'test.scss',
 			mappings: 'AAAA,SAAS,SAAS,EAAE',
 			names: [],
-			sourceRoot: '',
-			sources: [ '/folder/test.scss' ],
+			sources: [ 'test.scss' ],
 			sourcesContent: [ '.class { a: b c d; }' ],
 			version: 3
 		}]
