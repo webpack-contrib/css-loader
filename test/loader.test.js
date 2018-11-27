7cd50196b6f0c7e088b92e30cb8c7854690b0774
evilebottnawi <sheo13666q@gmail.com>
2018-11-27 19:19:01 +0300

    test: ensure empty options are working

--- test/loader.test.js
+++ test/loader.test.js
@@ -47,6 +47,21 @@ describe('loader', () => {
     expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
 
+  it('should compile with empty options', async () => {
+    const config = { loader: { options: {} } };
+    const testId = './empty.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+
+    expect(module.source).toMatchSnapshot('module');
+    expect(evaluated(module.source, modules)).toMatchSnapshot(
+      'module (evaluated)'
+    );
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
+
   it('should throws error when no loader for assets', async () => {
     const config = {
       rules: [
