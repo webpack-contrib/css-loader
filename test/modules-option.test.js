e081b42841e28aa1262146f9d1a0417e9625b1e8
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-11-28 00:09:15 +0300

    test: localsLoader (#816)

--- test/modules-option.test.js
+++ test/modules-option.test.js
@@ -7,24 +7,29 @@ const testCasesPath = path.join(__dirname, 'fixtures/modules/tests-cases');
 const testCases = fs.readdirSync(testCasesPath);
 
 describe('modules option', () => {
-  [true, false].forEach((optionValue) => {
-    testCases.forEach((name) => {
-      it(`case ${name} (option is ${optionValue})`, async () => {
-        const config = {
-          loader: {
-            options: { modules: optionValue, localIdentName: '_[local]' },
-          },
-        };
-        const testId = `./modules/tests-cases/${name}/source.css`;
-        const stats = await webpack(testId, config);
-        const { modules } = stats.toJson();
-        const module = modules.find((m) => m.id === testId);
-        const evaluatedModule = evaluated(module.source, modules);
+  [false, true].forEach((isLocalsLoader) => {
+    [false, true].forEach((modulesValue) => {
+      testCases.forEach((name) => {
+        it(`case name \`${name}\`: (use \`${
+          isLocalsLoader ? 'localsLoader.js' : 'loader.js'
+        }\`) (\`modules\` option is ${modulesValue})`, async () => {
+          const config = {
+            loader: {
+              options: { modules: modulesValue, localIdentName: '_[local]' },
+            },
+            localsLoader: isLocalsLoader,
+          };
+          const testId = `./modules/tests-cases/${name}/source.css`;
+          const stats = await webpack(testId, config);
+          const { modules } = stats.toJson();
+          const module = modules.find((m) => m.id === testId);
+          const evaluatedModule = evaluated(module.source, modules);
 
-        expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
-        expect(evaluatedModule.locals).toMatchSnapshot('locals');
-        expect(stats.compilation.warnings).toMatchSnapshot('warnings');
-        expect(stats.compilation.errors).toMatchSnapshot('errors');
+          expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
+          expect(evaluatedModule.locals).toMatchSnapshot('locals');
+          expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+          expect(stats.compilation.errors).toMatchSnapshot('errors');
+        });
       });
     });
   });
