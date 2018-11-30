947e666e4eca686b76078b5b3e72c365d019adbc
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-11-30 16:09:43 +0300

    refactor: drop getLocalIdent compatibility with webpack@2 and webpack@3 (#829)

--- test/getLocalIdent-option.test.js
+++ test/getLocalIdent-option.test.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const { webpack, evaluated } = require('./helpers');
 
 describe('getLocalIdent option', () => {
@@ -32,6 +34,62 @@ describe('getLocalIdent option', () => {
           getLocalIdent() {
             return 'foo';
           },
+        },
+      },
+    };
+    const testId = './modules/getLocalIdent.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+    const evaluatedModule = evaluated(module.source);
+
+    expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
+    expect(evaluatedModule.locals).toMatchSnapshot('locals');
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
+
+  it('should accepts arguments', async () => {
+    const config = {
+      loader: {
+        options: {
+          modules: true,
+          localIdentRegExp: 'regExp',
+          context: 'context',
+          hashPrefix: 'hash',
+          getLocalIdent(loaderContext, localIdentName, localName, options) {
+            expect(loaderContext).toBeDefined();
+            expect(typeof localIdentName).toBe('string');
+            expect(typeof localName).toBe('string');
+            expect(options).toBeDefined();
+
+            expect(options.regExp).toBe('regExp');
+            expect(options.context).toBe('context');
+            expect(options.hashPrefix).toBe('hash');
+
+            return 'foo';
+          },
+        },
+      },
+    };
+    const testId = './modules/getLocalIdent.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+    const evaluatedModule = evaluated(module.source);
+
+    expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
+    expect(evaluatedModule.locals).toMatchSnapshot('locals');
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
+
+  it('should respect `context` option', async () => {
+    const config = {
+      loader: {
+        options: {
+          context: path.resolve(__dirname, 'fixtures/modules'),
+          modules: true,
         },
       },
     };
