5bf75867309403a8862f70f637bf6a00bbb6f12c
evilebottnawi <sheo13666q@gmail.com>
2018-12-04 13:36:08 +0300

    fix: url resolving logic
    BREAKING CHANGE: resolving logic works the same everywhere (it does not matter whether css modules are enabled or not or you setup `global` or `local` mode). Examples -
    `url('image.png')` as `require('./image.png')`, `url('./image.png')` as `require('./image.png')`, `url('~module/image.png')` as `require('module/image.png')`.

--- test/url-option.test.js
+++ test/url-option.test.js
@@ -31,4 +31,27 @@ describe('url option', () => {
     expect(stats.compilation.warnings).toMatchSnapshot('warnings');
     expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
+
+  [true, 'local', 'global', false].forEach((modulesValue) => {
+    it(`true and modules \`${modulesValue}\``, async () => {
+      const config = {
+        loader: { options: { modules: modulesValue } },
+      };
+      const testId = './url/url.css';
+      const stats = await webpack(testId, config);
+      const { modules } = stats.toJson();
+      const module = modules.find((m) => m.id === testId);
+
+      expect(module.source).toMatchSnapshot('module');
+      expect(evaluated(module.source, modules)).toMatchSnapshot(
+        'module (evaluated)'
+      );
+      expect(normalizeErrors(stats.compilation.warnings)).toMatchSnapshot(
+        'warnings'
+      );
+      expect(normalizeErrors(stats.compilation.errors)).toMatchSnapshot(
+        'errors'
+      );
+    });
+  });
 });
