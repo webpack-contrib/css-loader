12f88e2af0cd60bed62dd4b34da538e6470287f6
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2018-12-10 19:05:36 +0300

    test: resolving in node_modules (#866)

--- test/modules-option.test.js
+++ test/modules-option.test.js
@@ -117,4 +117,21 @@ describe('modules', () => {
     expect(stats.compilation.warnings).toMatchSnapshot('warnings');
     expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
+
+  it('issue #861', async () => {
+    const config = {
+      loader: { options: { modules: true } },
+    };
+    const testId = './modules/resolving-inside-node-modules.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+
+    expect(module.source).toMatchSnapshot('module');
+    expect(evaluated(module.source, modules)).toMatchSnapshot(
+      'module (evaluated)'
+    );
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
 });
