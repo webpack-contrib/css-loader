888cca02275f92c72db6ba3ae535174ba482652c
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-05-28 20:06:13 +0300

    feat: replace `camelCase` option on `exportLocalsStyle` option (#938)
    BREAKING CHANGE: `camelCase` option was removed in favor `exportLocalsStyle` option. Option value should be `String`. Use `camelCase` if you option value was `true` and `asIs` if you option value was `false`.

--- test/exportLocalsStyle-option.test.js
+++ test/exportLocalsStyle-option.test.js
@@ -1,16 +1,9 @@
 import { webpack, evaluated } from './helpers';
 
-describe('camelCase option', () => {
-  it('true', async () => {
-    const config = {
-      loader: {
-        options: {
-          modules: true,
-          camelCase: true,
-        },
-      },
-    };
-    const testId = './modules/camelCase.css';
+describe('exportLocalsStyle option', () => {
+  it('not specified', async () => {
+    const config = { loader: { options: { modules: true } } };
+    const testId = './modules/exportLocalsStyle.css';
     const stats = await webpack(testId, config);
     const { modules } = stats.toJson();
     const module = modules.find((m) => m.id === testId);
@@ -22,9 +15,58 @@ describe('camelCase option', () => {
     expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
 
-  it('not specified', async () => {
-    const config = { loader: { options: { modules: true } } };
-    const testId = './modules/camelCase.css';
+  it('asIs', async () => {
+    const config = {
+      loader: {
+        options: {
+          modules: true,
+          exportLocalsStyle: 'asIs',
+        },
+      },
+    };
+    const testId = './modules/exportLocalsStyle.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+    const evaluatedModule = evaluated(module.source);
+
+    expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
+    expect(evaluatedModule.locals).toMatchSnapshot('locals');
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
+
+  it('camelCase', async () => {
+    const config = {
+      loader: {
+        options: {
+          modules: true,
+          exportLocalsStyle: 'camelCase',
+        },
+      },
+    };
+    const testId = './modules/exportLocalsStyle.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+    const evaluatedModule = evaluated(module.source);
+
+    expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
+    expect(evaluatedModule.locals).toMatchSnapshot('locals');
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
+
+  it('camelCaseOnly', async () => {
+    const config = {
+      loader: {
+        options: {
+          modules: true,
+          exportLocalsStyle: 'camelCaseOnly',
+        },
+      },
+    };
+    const testId = './modules/exportLocalsStyle.css';
     const stats = await webpack(testId, config);
     const { modules } = stats.toJson();
     const module = modules.find((m) => m.id === testId);
@@ -41,32 +83,11 @@ describe('camelCase option', () => {
       loader: {
         options: {
           modules: true,
-          camelCase: 'dashes',
+          exportLocalsStyle: 'dashes',
         },
       },
     };
-    const testId = './modules/camelCase.css';
-    const stats = await webpack(testId, config);
-    const { modules } = stats.toJson();
-    const module = modules.find((m) => m.id === testId);
-    const evaluatedModule = evaluated(module.source);
-
-    expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
-    expect(evaluatedModule.locals).toMatchSnapshot('locals');
-    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
-    expect(stats.compilation.errors).toMatchSnapshot('errors');
-  });
-
-  it('only', async () => {
-    const config = {
-      loader: {
-        options: {
-          modules: true,
-          camelCase: 'only',
-        },
-      },
-    };
-    const testId = './modules/camelCase.css';
+    const testId = './modules/exportLocalsStyle.css';
     const stats = await webpack(testId, config);
     const { modules } = stats.toJson();
     const module = modules.find((m) => m.id === testId);
@@ -83,11 +104,11 @@ describe('camelCase option', () => {
       loader: {
         options: {
           modules: true,
-          camelCase: 'dashesOnly',
+          exportLocalsStyle: 'dashesOnly',
         },
       },
     };
-    const testId = './modules/camelCase.css';
+    const testId = './modules/exportLocalsStyle.css';
     const stats = await webpack(testId, config);
     const { modules } = stats.toJson();
     const module = modules.find((m) => m.id === testId);
