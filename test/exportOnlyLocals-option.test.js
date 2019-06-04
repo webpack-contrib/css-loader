456c5ed0e21b79b308a4b99fcf66423591c7af76
evilebottnawi <sheo13666q@gmail.com>
2019-06-04 16:24:17 +0300

    refactor: code and tests

---
+++ test/exportOnlyLocals-option.test.js
@@ -0,0 +1,29 @@
+import { webpack } from './helpers';
+
+describe('modules', () => {
+  it('true (mode: local)', async () => {
+    const config = {
+      loader: {
+        options: {
+          modules: {
+            mode: 'local',
+            localIdentName: '_[local]',
+          },
+          exportOnlyLocals: true,
+        },
+      },
+    };
+    const testId = `./modules/composes.css`;
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+    const valueModule = modules.find((m) =>
+      m.id.endsWith('./modules/values.css')
+    );
+
+    expect(module.source).toMatchSnapshot('module');
+    expect(valueModule.source).toMatchSnapshot('values module');
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
+});
