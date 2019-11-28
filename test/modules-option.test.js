0961304020832fc9ca70cc708f4366e1f868e765
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-11-28 13:29:56 +0300

    fix: add an additional space after the escape sequence (#998)

--- test/modules-option.test.js
+++ test/modules-option.test.js
@@ -532,4 +532,28 @@ describe('modules', () => {
     expect(stats.compilation.warnings).toMatchSnapshot('warnings');
     expect(stats.compilation.errors).toMatchSnapshot('errors');
   });
+
+  it('issue #995', async () => {
+    const config = {
+      loader: {
+        options: {
+          modules: {
+            mode: 'global',
+            localIdentName: '😀',
+          },
+        },
+      },
+    };
+    const testId = './modules/issue-995.css';
+    const stats = await webpack(testId, config);
+    const { modules } = stats.toJson();
+    const module = modules.find((m) => m.id === testId);
+
+    expect(module.source).toMatchSnapshot('module');
+    expect(evaluated(module.source, modules)).toMatchSnapshot(
+      'module (evaluated)'
+    );
+    expect(stats.compilation.warnings).toMatchSnapshot('warnings');
+    expect(stats.compilation.errors).toMatchSnapshot('errors');
+  });
 });
