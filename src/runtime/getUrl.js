a646ca1a4acccf729ca8df925d185f74adb21e95
evilebottnawi <sheo13666q@gmail.com>
2019-12-02 16:01:46 +0300

    fix: compatibility with ES modules syntax and hash in `url` function

--- src/runtime/getUrl.js
+++ src/runtime/getUrl.js
@@ -1,4 +1,9 @@
-module.exports = (url, needQuotes) => {
+module.exports = (url, options) => {
+  if (!options) {
+    // eslint-disable-next-line no-param-reassign
+    options = {};
+  }
+
   // eslint-disable-next-line no-underscore-dangle, no-param-reassign
   url = url.__esModule ? url.default : url;
 
@@ -12,9 +17,14 @@ module.exports = (url, needQuotes) => {
     url = url.slice(1, -1);
   }
 
+  if (options.hash) {
+    // eslint-disable-next-line no-param-reassign
+    url += options.hash;
+  }
+
   // Should url be wrapped?
   // See https://drafts.csswg.org/css-values-3/#urls
-  if (/["'() \t\n]/.test(url) || needQuotes) {
+  if (/["'() \t\n]/.test(url) || options.needQuotes) {
     return `"${url.replace(/"/g, '\\"').replace(/\n/g, '\\n')}"`;
   }
 
