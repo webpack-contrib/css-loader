67697833725e1cff12a14663390bbe4c65ea36d2
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-12-06 17:16:37 +0300

    fix: do not crash when an assert return `null` or `undefined` (#1006)

--- src/runtime/getUrl.js
+++ src/runtime/getUrl.js
@@ -5,7 +5,7 @@ module.exports = (url, options) => {
   }
 
   // eslint-disable-next-line no-underscore-dangle, no-param-reassign
-  url = url.__esModule ? url.default : url;
+  url = url && url.__esModule ? url.default : url;
 
   if (typeof url !== 'string') {
     return url;
