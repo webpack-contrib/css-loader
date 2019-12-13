a46ce850e834ff742a95733dc00552f9ef4b2002
evilebottnawi <sheo13666q@gmail.com>
2019-12-13 18:29:21 +0300

    test: refactor icss

--- test/icss.test.js
+++ test/icss.test.js
@@ -1,8 +1,15 @@
 import path from 'path';
 import fs from 'fs';
 
-import { webpack, evaluated } from './helpers';
-import { getErrors, getWarnings } from './helpers/index';
+import {
+  compile,
+  execute,
+  getCompiler,
+  getErrors,
+  getModuleSource,
+  getWarnings,
+  readAsset,
+} from './helpers/index';
 
 const testCasesPath = path.join(__dirname, 'fixtures/icss/tests-cases');
 const testCases = fs.readdirSync(testCasesPath);
@@ -10,14 +17,15 @@ const testCases = fs.readdirSync(testCasesPath);
 describe('ICSS', () => {
   testCases.forEach((name) => {
     it(`case ${name}`, async () => {
-      const testId = `./icss/tests-cases/${name}/source.css`;
-      const stats = await webpack(testId);
-      const { modules } = stats.toJson();
-      const module = modules.find((m) => m.id === testId);
-      const evaluatedModule = evaluated(module.source, modules);
+      const compiler = getCompiler(`./icss/tests-cases/${name}/source.js`);
+      const stats = await compile(compiler);
 
-      expect(evaluatedModule).toMatchSnapshot('module (evaluated)');
-      expect(evaluatedModule.locals).toMatchSnapshot('locals');
+      expect(
+        getModuleSource(`./icss/tests-cases/${name}/source.css`, stats)
+      ).toMatchSnapshot('module');
+      expect(
+        execute(readAsset('main.bundle.js', compiler, stats))
+      ).toMatchSnapshot('result');
       expect(getWarnings(stats)).toMatchSnapshot('warnings');
       expect(getErrors(stats)).toMatchSnapshot('errors');
     });
