27c440cf68b3c6c84d091fee0248f56debaf5629
evilebottnawi <sheo13666q@gmail.com>
2019-12-16 15:57:09 +0300

    test: refactor

--- test/helpers/getModuleSource.js
+++ test/helpers/getModuleSource.js
@@ -1,5 +1,5 @@
 export default (id, stats) => {
-  const { modules } = stats.toJson();
+  const { modules } = stats.toJson({ source: true });
   const module = modules.find((m) => m.id === id);
 
   return module.source;
