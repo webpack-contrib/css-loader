fc83d34cb29ceab27ab3c71f71b4576f2f1666b9
denisx <github.com@denisx.com>
2020-04-03 00:06:19 +0300

    save

--- src/plugins/one-letter-css.js
+++ src/plugins/one-letter-css.js
@@ -2,25 +2,34 @@
  * @author denisx <github.com@denisx.com>
  */
 
-import { interpolateName } from 'loader-utils';
+const { interpolateName } = require('loader-utils');
 
-export default class OneLetterCss {
+class OneLetterCss {
   constructor() {
     // Save char symbol start positions
     this.a = 'a'.charCodeAt(0);
     this.A = 'A'.charCodeAt(0);
+    this.zero = '0'.charCodeAt(0);
     // file hashes cache
     this.files = {};
     /** encoding [a-zA-Z] */
-    this.symbols = 52;
+    this.symbols = 64;
     /** a half of encoding */
-    this.half = 26;
+    // this.half = 26;
+    this.symbolsAreaEnd = {
+      az: 26,
+      AZ: 52,
+      _: 53,
+      // 0-9
+      d: 63,
+      // -
+      dash: 64,
+    };
     /** prevent loop-hell */
     this.maxLoop = 10;
   }
 
-  /** encoding by rule count at file, 0 - a, 1 - b, 51 - Z, 52 - ba, etc */
-  getName(lastUsed) {
+  getNameOld(lastUsed) {
     const { a, A, symbols, maxLoop, half } = this;
     let name = '';
     let loop = 0;
@@ -42,6 +51,72 @@ export default class OneLetterCss {
     }
 
     return name;
+  }
+
+  /**
+   * 1st set [_a-zA-Z]
+   * 2nd set [_a-zA-Z0-9-]
+   *
+   * */
+  getName(lastUsed) {
+    if (!lastUsed) {
+      return '';
+    }
+
+    const { a, A, symbols, maxLoop, zero } = this;
+    let name = '';
+    let loop = 0;
+    let main = lastUsed;
+    let tail = 0;
+
+    while (
+      main > 0 &&
+      // && tail >= 0
+      // ||
+      // first step anyway needed
+      // loop === 0
+      loop < maxLoop
+    ) {
+      const newMain = Math.floor(main / symbols);
+
+      tail = main % symbols;
+
+      // if (tail === -1) {
+      //   newMain -= 1
+      // }
+
+      let tmpName;
+
+      //       az: 26,
+      //       AZ: 52,
+      //       _: 53,
+      //       d: 63,
+      //       dash: 64
+      if (tail === -1) {
+        tmpName = '-';
+      } else if (tail <= this.symbolsAreaEnd.az) {
+        tmpName = String.fromCharCode(a + tail - 1);
+      } else if (tail <= this.symbolsAreaEnd.AZ) {
+        tmpName = String.fromCharCode(A - this.symbolsAreaEnd.az + tail - 1);
+      } else if (tail <= this.symbolsAreaEnd._) {
+        tmpName = '_';
+      } else if (tail <= this.symbolsAreaEnd.d) {
+        tmpName = String.fromCharCode(
+          zero - this.symbolsAreaEnd.AZ - 1 + tail - 1
+        );
+      } else {
+        throw new Error(
+          `tail value > dash; ${tail} > ${this.symbolsAreaEnd.dash}`
+        );
+      }
+
+      name = tmpName + name;
+
+      main = newMain;
+      loop += 1;
+    }
+
+    return (/^[\d-]/.test(name) ? '_' : '') + name;
   }
 
   getLocalIdent(context, localIdentName, localName) {
@@ -86,3 +161,5 @@ export default class OneLetterCss {
     return hasLocal ? `${localName}__${newRuleName}` : newRuleName;
   }
 }
+
+module.exports = OneLetterCss;
--- test/one-letter-css.test.js
+++ test/one-letter-css.test.js
@@ -1,98 +1,170 @@
-import { OneLetterCss } from '../src/plugins';
+const OneLetterCss = require('../src/plugins/one-letter-css');
 
 /* webpack set */
-const workSets = [
-  {
-    in: [
-      {
-        resourcePath: './file1.css',
-      },
-      '[hash:base64:8]',
-      'theme-white',
-    ],
-    out: ['a2zADNwsK'],
-  },
-  {
-    in: [
-      {
-        resourcePath: './file1.css',
-      },
-      '[hash:base64:8]',
-      'theme-blue',
-    ],
-    out: ['b2zADNwsK'],
-  },
-  {
-    in: [
-      {
-        resourcePath: './file2.css',
-      },
-      '[hash:base64:8]',
-      'text-white',
-    ],
-    out: ['a2jlx459O'],
-  },
-  {
-    in: [
-      {
-        resourcePath: './file2.css',
-      },
-      '[hash:base64:8]',
-      'text-blue',
-    ],
-    out: ['b2jlx459O'],
-  },
-  // for develop case
-  {
-    in: [
-      {
-        resourcePath: './file2.css',
-      },
-      '[local]__[hash:base64:8]',
-      'text-blue',
-    ],
-    out: ['text-blue__b2jlx459O'],
-  },
-];
+// const workSets = [
+//   {
+//     in: [
+//       {
+//         resourcePath: './file1.css',
+//       },
+//       '[hash:base64:8]',
+//       'theme-white',
+//     ],
+//     out: 'a2zADNwsK',
+//   },
+//   {
+//     in: [
+//       {
+//         resourcePath: './file1.css',
+//       },
+//       '[hash:base64:8]',
+//       'theme-blue',
+//     ],
+//     out: 'b2zADNwsK',
+//   },
+//   {
+//     in: [
+//       {
+//         resourcePath: './file2.css',
+//       },
+//       '[hash:base64:8]',
+//       'text-white',
+//     ],
+//     out: 'a2jlx459O',
+//   },
+//   {
+//     in: [
+//       {
+//         resourcePath: './file2.css',
+//       },
+//       '[hash:base64:8]',
+//       'text-blue',
+//     ],
+//     out: 'b2jlx459O',
+//   },
+//   // for develop case
+//   {
+//     in: [
+//       {
+//         resourcePath: './file2.css',
+//       },
+//       '[local]__[hash:base64:8]',
+//       'text-blue',
+//     ],
+//     out: 'text-blue__b2jlx459O',
+//   },
+// ];
 
 /* encoding test set */
 const encodingSets = [
   {
-    in: [0],
-    out: ['a'],
+    in: 0,
+    out: '',
   },
   {
-    in: [1],
-    out: ['b'],
+    in: 1,
+    out: 'a',
   },
   {
-    in: [51],
-    out: ['Z'],
+    in: 2,
+    out: 'b',
   },
   {
-    in: [52],
-    out: ['ba'],
+    in: 25,
+    out: 'y',
   },
   {
-    in: [53],
-    out: ['bb'],
+    in: 26,
+    out: 'z',
+  },
+  {
+    in: 27,
+    out: 'A',
+  },
+  {
+    in: 28,
+    out: 'B',
+  },
+  {
+    in: 51,
+    out: 'Y',
+  },
+  {
+    in: 52,
+    out: 'Z',
+  },
+  {
+    in: 53,
+    out: '_',
+  },
+  {
+    in: 54,
+    out: '_0',
+  },
+  {
+    in: 55,
+    out: '_1',
+  },
+  {
+    in: 62,
+    out: '_8',
+  },
+  {
+    in: 63,
+    out: '_9',
+  },
+  {
+    in: 64,
+    out: '_-',
+  },
+  {
+    in: 65,
+    out: 'aa',
+  },
+  {
+    in: 66,
+    out: 'ab',
+  },
+  {
+    in: 67,
+    out: 'ac',
+  },
+  {
+    in: 4095,
+    out: '_-9',
+  },
+  {
+    in: 4096,
+    out: '_--',
+  },
+  {
+    in: 4097,
+    out: 'aaa',
   },
 ];
 
 const MyOneLetterCss = new OneLetterCss();
 
-describe('testing work case', () => {
-  workSets.forEach((set) => {
-    it(`should check name generate`, () => {
-      expect(MyOneLetterCss.getLocalIdent(...set.in)).toEqual(...set.out);
-    });
-  });
+// describe('testing work case', () => {
+//   workSets.forEach((set) => {
+//     it(`should check name generate`, () => {
+//       expect(MyOneLetterCss.getLocalIdent(...set.in)).toEqual(...set.out);
+//     });
+//   });
+// });
+
+let ok = 0;
+
+encodingSets.forEach((set) => {
+  if (MyOneLetterCss.getName(set.in) === set.out) {
+    ok += 1;
+  } else {
+    console.log('---');
+    console.log('  send', set.in);
+    console.log('expect', set.out);
+    console.log('   get', MyOneLetterCss.getName(set.in));
+    // throw new Error()
+  }
 });
 
-describe('testing encoding method', () => {
-  encodingSets.forEach((set) => {
-    it(`should check name generate`, () => {
-      expect(MyOneLetterCss.getName(...set.in)).toEqual(...set.out);
-    });
-  });
-});
+console.log('ok=', ok);
