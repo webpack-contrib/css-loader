649111d20902d6689899fc50534232697bc0cc25
denisx <github.com@denisx.com>
2020-04-25 21:00:59 +0300

    emprove encoding from 53 to 64 bits

--- src/plugins/one-letter-css.js
+++ src/plugins/one-letter-css.js
@@ -12,125 +12,116 @@ class OneLetterCss {
     this.zero = '0'.charCodeAt(0);
     // file hashes cache
     this.files = {};
-    /** encoding [a-zA-Z] */
-    this.symbols = 64;
-    /** a half of encoding */
-    // this.half = 26;
-    this.symbolsAreaEnd = {
+    // encoding [a-zA-Z\d_-]
+    this.encoderSize = 64;
+    this.symbolsArea = {
+      // a-z
       az: 26,
+      // A-Z
       AZ: 52,
-      _: 53,
-      // 0-9
-      d: 63,
+      // _
+      under: 53,
+      // 0-9 | \d
+      digit: 63,
       // -
       dash: 64,
     };
-    /** prevent loop-hell */
-    this.maxLoop = 10;
+    // prevent loop hell
+    this.maxLoop = 5;
   }
 
-  getNameOld(lastUsed) {
-    const { a, A, symbols, maxLoop, half } = this;
-    let name = '';
-    let loop = 0;
-    let main = lastUsed;
-    let tail = 0;
+  getSingleSymbol(n) {
+    const {
+      a,
+      A,
+      zero,
+      encoderSize,
+      symbolsArea: { az, AZ, under, digit },
+    } = this;
 
-    while (
-      ((main > 0 && tail >= 0) ||
-        // first step anyway needed
-        loop === 0) &&
-      loop < maxLoop
-    ) {
-      const newMain = Math.floor(main / symbols);
-
-      tail = main % symbols;
-      name = String.fromCharCode((tail >= half ? A - half : a) + tail) + name;
-      main = newMain;
-      loop += 1;
-    }
-
-    return name;
-  }
-
-  /**
-   * 1st set [_a-zA-Z]
-   * 2nd set [_a-zA-Z0-9-]
-   *
-   * */
-  getName(lastUsed) {
-    if (!lastUsed) {
+    if (!n) {
+      console.error(`!n, n=${n}`);
       return '';
     }
 
-    const { a, A, symbols, maxLoop, zero } = this;
-    let name = '';
-    let loop = 0;
-    let main = lastUsed;
-    let tail = 0;
-
-    while (
-      main > 0 &&
-      // && tail >= 0
-      // ||
-      // first step anyway needed
-      // loop === 0
-      loop < maxLoop
-    ) {
-      const newMain = Math.floor(main / symbols);
-
-      tail = main % symbols;
-
-      // if (tail === -1) {
-      //   newMain -= 1
-      // }
-
-      let tmpName;
-
-      //       az: 26,
-      //       AZ: 52,
-      //       _: 53,
-      //       d: 63,
-      //       dash: 64
-      if (tail === -1) {
-        tmpName = '-';
-      } else if (tail <= this.symbolsAreaEnd.az) {
-        tmpName = String.fromCharCode(a + tail - 1);
-      } else if (tail <= this.symbolsAreaEnd.AZ) {
-        tmpName = String.fromCharCode(A - this.symbolsAreaEnd.az + tail - 1);
-      } else if (tail <= this.symbolsAreaEnd._) {
-        tmpName = '_';
-      } else if (tail <= this.symbolsAreaEnd.d) {
-        tmpName = String.fromCharCode(
-          zero - this.symbolsAreaEnd.AZ - 1 + tail - 1
-        );
-      } else {
-        throw new Error(
-          `tail value > dash; ${tail} > ${this.symbolsAreaEnd.dash}`
-        );
-      }
-
-      name = tmpName + name;
-
-      main = newMain;
-      loop += 1;
+    if (n > encoderSize) {
+      console.error(`n > ${encoderSize}, n=${n}`);
+      return '';
     }
 
-    return (/^[\d-]/.test(name) ? '_' : '') + name;
+    // work with 1 <= n <= 64
+    if (n <= az) {
+      return String.fromCharCode(n - 1 + a);
+    }
+
+    if (n <= AZ) {
+      return String.fromCharCode(n - 1 - az + A);
+    }
+
+    if (n <= under) {
+      return '_';
+    }
+
+    if (n <= digit) {
+      return String.fromCharCode(n - 1 - under + zero);
+    }
+
+    return '-';
+  }
+
+  getNamePrefix(num) {
+    const { maxLoop, encoderSize } = this;
+
+    if (!num) {
+      return '';
+    }
+
+    let loopCount = 0;
+    let n = num;
+    let res = '';
+
+    while (n && loopCount < maxLoop) {
+      let tail = n % encoderSize;
+      const origTail = tail;
+
+      // If n === encoderSize
+      if (tail === 0) {
+        tail = encoderSize;
+      }
+
+      res = this.getSingleSymbol(tail) + res;
+
+      if (Math.floor((n - 1) / encoderSize)) {
+        n = (n - origTail) / encoderSize;
+
+        if (origTail === 0) {
+          n -= 1;
+        }
+      } else {
+        n = 0;
+      }
+
+      loopCount += 1;
+    }
+
+    // Add prefix '_' for css-names, started with '-' or digit '\d'
+    return /^[\d-]/.test(res) ? `_${res}` : res;
   }
 
   getLocalIdent(context, localIdentName, localName) {
     const { resourcePath } = context;
     const { files } = this;
 
-    // check file data at cache by absolute path
+    // Check file data at cache by absolute path
     let fileShort = files[resourcePath];
 
     // no file data, lets generate and save
     if (!fileShort) {
-      // if we know file position, we must use base52 encoding with '_'
+      // If make file position encoding, use base64 with '_'
       // between rule position and file position
       // to avoid collapse hash combination. a_ab vs aa_b
+
+      // Make encoding with filepath content hash
       const fileShortName = interpolateName(context, '[hash:base64:8]', {
         content: resourcePath,
       });
@@ -148,9 +139,9 @@ class OneLetterCss {
       fileShort.lastUsed += 1;
 
       // Generate new rule name
-      newRuleName = this.getName(fileShort.lastUsed) + fileShort.name;
+      newRuleName = this.getNamePrefix(fileShort.lastUsed) + fileShort.name;
 
-      // Saved
+      // Save
       fileShort.ruleNames[localName] = newRuleName;
     }
 
--- test/one-letter-css.test.js
+++ test/one-letter-css.test.js
@@ -1,170 +1,114 @@
 const OneLetterCss = require('../src/plugins/one-letter-css');
 
 /* webpack set */
-// const workSets = [
-//   {
-//     in: [
-//       {
-//         resourcePath: './file1.css',
-//       },
-//       '[hash:base64:8]',
-//       'theme-white',
-//     ],
-//     out: 'a2zADNwsK',
-//   },
-//   {
-//     in: [
-//       {
-//         resourcePath: './file1.css',
-//       },
-//       '[hash:base64:8]',
-//       'theme-blue',
-//     ],
-//     out: 'b2zADNwsK',
-//   },
-//   {
-//     in: [
-//       {
-//         resourcePath: './file2.css',
-//       },
-//       '[hash:base64:8]',
-//       'text-white',
-//     ],
-//     out: 'a2jlx459O',
-//   },
-//   {
-//     in: [
-//       {
-//         resourcePath: './file2.css',
-//       },
-//       '[hash:base64:8]',
-//       'text-blue',
-//     ],
-//     out: 'b2jlx459O',
-//   },
-//   // for develop case
-//   {
-//     in: [
-//       {
-//         resourcePath: './file2.css',
-//       },
-//       '[local]__[hash:base64:8]',
-//       'text-blue',
-//     ],
-//     out: 'text-blue__b2jlx459O',
-//   },
-// ];
+const workSets = [
+  {
+    in: [
+      {
+        resourcePath: './file1.css',
+      },
+      '[hash:base64:8]',
+      'theme-white',
+    ],
+    out: '2zADNwsK',
+  },
+  {
+    in: [
+      {
+        resourcePath: './file1.css',
+      },
+      '[hash:base64:8]',
+      'theme-blue',
+    ],
+    out: 'a2zADNwsK',
+  },
+  {
+    in: [
+      {
+        resourcePath: './file2.css',
+      },
+      '[hash:base64:8]',
+      'text-white',
+    ],
+    out: '2jlx459O',
+  },
+  {
+    in: [
+      {
+        resourcePath: './file2.css',
+      },
+      '[hash:base64:8]',
+      'text-blue',
+    ],
+    out: 'a2jlx459O',
+  },
+  // for develop case
+  {
+    in: [
+      {
+        resourcePath: './file2.css',
+      },
+      '[local]__[hash:base64:8]',
+      'text-blue',
+    ],
+    out: 'text-blue__a2jlx459O',
+  },
+];
 
 /* encoding test set */
 const encodingSets = [
-  {
-    in: 0,
-    out: '',
-  },
-  {
-    in: 1,
-    out: 'a',
-  },
-  {
-    in: 2,
-    out: 'b',
-  },
-  {
-    in: 25,
-    out: 'y',
-  },
-  {
-    in: 26,
-    out: 'z',
-  },
-  {
-    in: 27,
-    out: 'A',
-  },
-  {
-    in: 28,
-    out: 'B',
-  },
-  {
-    in: 51,
-    out: 'Y',
-  },
-  {
-    in: 52,
-    out: 'Z',
-  },
-  {
-    in: 53,
-    out: '_',
-  },
-  {
-    in: 54,
-    out: '_0',
-  },
-  {
-    in: 55,
-    out: '_1',
-  },
-  {
-    in: 62,
-    out: '_8',
-  },
-  {
-    in: 63,
-    out: '_9',
-  },
-  {
-    in: 64,
-    out: '_-',
-  },
-  {
-    in: 65,
-    out: 'aa',
-  },
-  {
-    in: 66,
-    out: 'ab',
-  },
-  {
-    in: 67,
-    out: 'ac',
-  },
-  {
-    in: 4095,
-    out: '_-9',
-  },
-  {
-    in: 4096,
-    out: '_--',
-  },
-  {
-    in: 4097,
-    out: 'aaa',
-  },
+  // [in, out]
+  // it's ok, it's a prefix, can be empty
+  [0, ''],
+  [1, 'a'],
+  [2, 'b'],
+  [25, 'y'],
+  [26, 'z'],
+  [27, 'A'],
+  [28, 'B'],
+  [51, 'Y'],
+  [52, 'Z'],
+  [53, '_'],
+  [54, '_0'],
+  [55, '_1'],
+  [62, '_8'],
+  [63, '_9'],
+  [64, '_-'],
+  [65, 'aa'],
+  [66, 'ab'],
+  [116, 'aZ'],
+  [117, 'a_'],
+  [118, 'a0'],
+  [127, 'a9'],
+  [128, 'a-'],
+  [129, 'ba'],
+  [130, 'bb'],
+  [190, 'b8'],
+  [191, 'b9'],
+  [192, 'b-'],
+  [193, 'ca'],
+  [194, 'cb'],
+  [4158, '_-8'],
+  [4159, '_-9'],
+  // last 2-symbols part, '--', 64*65 (64*64 2-symbols + 64 1-letter)
+  [4160, '_--'],
+  // start 3-symbols part
+  [4161, 'aaa'],
+  [4162, 'aab'],
 ];
 
 const MyOneLetterCss = new OneLetterCss();
 
-// describe('testing work case', () => {
-//   workSets.forEach((set) => {
-//     it(`should check name generate`, () => {
-//       expect(MyOneLetterCss.getLocalIdent(...set.in)).toEqual(...set.out);
-//     });
-//   });
-// });
+describe('testing work cases', () => {
+  workSets.forEach((set) => {
+    it(`should check classname full generate`, () => {
+      expect(MyOneLetterCss.getLocalIdent(...set.in)).toEqual(set.out);
+    });
+  });
 
-let ok = 0;
-
-encodingSets.forEach((set) => {
-  if (MyOneLetterCss.getName(set.in) === set.out) {
-    ok += 1;
-  } else {
-    console.log('---');
-    console.log('  send', set.in);
-    console.log('expect', set.out);
-    console.log('   get', MyOneLetterCss.getName(set.in));
-    // throw new Error()
-  }
+  encodingSets.forEach(([valIn, valOut]) => {
+    it(`should check classname prefix generate`, () => {
+      expect(MyOneLetterCss.getNamePrefix(valIn)).toEqual(valOut);
+    });
+  });
 });
-
-console.log('ok=', ok);
