21464fb7a81478c009bb6502ece3d9da8b2997f5
evilebottnawi <sheo13666q@gmail.com>
2020-07-20 21:38:10 +0300

    refactor: code

--- src/plugins/postcss-icss-parser.js
+++ src/plugins/postcss-icss-parser.js
@@ -1,134 +1,90 @@
 import postcss from 'postcss';
 import { extractICSS, replaceValueSymbols, replaceSymbols } from 'icss-utils';
 
-import {
-  normalizeUrl,
-  resolveRequests,
-  isUrlRequestable,
-  requestify,
-} from '../utils';
-
-function makeRequestableIcssImports(icssImports, rootContext) {
-  return Object.keys(icssImports).reduce((accumulator, url) => {
-    const tokensMap = icssImports[url];
-    const tokens = Object.keys(tokensMap);
-
-    if (tokens.length === 0) {
-      return accumulator;
-    }
-
-    const isRequestable = isUrlRequestable(url);
-
-    let normalizedUrl;
-
-    if (isRequestable) {
-      normalizedUrl = requestify(normalizeUrl(url, true), rootContext);
-    }
-
-    const key = typeof normalizedUrl !== 'undefined' ? normalizedUrl : url;
-
-    if (!accumulator[key]) {
-      // eslint-disable-next-line no-param-reassign
-      accumulator[key] = { url, tokenMap: tokensMap };
-    } else {
-      // eslint-disable-next-line no-param-reassign
-      accumulator[key] = {
-        url,
-        tokenMap: {
-          ...accumulator[key].tokenMap,
-          ...tokensMap,
-        },
-      };
-    }
-
-    return accumulator;
-  }, {});
-}
+import { normalizeUrl, resolveRequests, requestify } from '../utils';
 
 export default postcss.plugin(
   'postcss-icss-parser',
   (options) => async (css, result) => {
     const importReplacements = Object.create(null);
-    const extractedICSS = extractICSS(css);
-    const icssImports = makeRequestableIcssImports(
-      extractedICSS.icssImports,
-      options.rootContext
-    );
-
+    const { icssImports, icssExports } = extractICSS(css);
+    const imports = new Map();
     const tasks = [];
 
-    let index = 0;
+    // eslint-disable-next-line guard-for-in
+    for (const url in icssImports) {
+      const tokens = icssImports[url];
 
-    for (const [importIndex, normalizedUrl] of Object.keys(
-      icssImports
-    ).entries()) {
-      const { url } = icssImports[normalizedUrl];
+      if (Object.keys(tokens).length === 0) {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
 
-      index += 1;
+      const request = requestify(normalizeUrl(url, true), options.rootContext);
+      const doResolve = async () => {
+        const { resolver, context } = options;
+        const resolvedUrl = await resolveRequests(resolver, context, [
+          ...new Set([url, request]),
+        ]);
 
-      tasks.push(
-        Promise.resolve(index).then(async (currentIndex) => {
-          const importName = `___CSS_LOADER_ICSS_IMPORT_${importIndex}___`;
-          const { resolver, context } = options;
+        return { url: resolvedUrl, tokens };
+      };
 
-          let resolvedUrl;
-
-          try {
-            resolvedUrl = await resolveRequests(resolver, context, [
-              ...new Set([normalizedUrl, url]),
-            ]);
-          } catch (error) {
-            throw error;
-          }
-
-          result.messages.push(
-            {
-              type: 'import',
-              value: {
-                order: 0,
-                icss: true,
-                importName,
-                url: options.urlHandler(resolvedUrl),
-                index: currentIndex,
-              },
-            },
-            {
-              type: 'api-import',
-              value: {
-                order: 0,
-                type: 'internal',
-                importName,
-                dedupe: true,
-                index: currentIndex,
-              },
-            }
-          );
-
-          const { tokenMap } = icssImports[normalizedUrl];
-          const tokens = Object.keys(tokenMap);
-
-          for (const [replacementIndex, token] of tokens.entries()) {
-            const replacementName = `___CSS_LOADER_ICSS_IMPORT_${importIndex}_REPLACEMENT_${replacementIndex}___`;
-            const localName = tokenMap[token];
-
-            importReplacements[token] = replacementName;
-
-            result.messages.push({
-              type: 'icss-replacement',
-              value: { replacementName, importName, localName },
-            });
-          }
-        })
-      );
+      tasks.push(doResolve());
     }
 
-    await Promise.all(tasks);
+    const results = await Promise.all(tasks);
+
+    for (let index = 0; index <= results.length - 1; index++) {
+      const { url, tokens } = results[index];
+
+      const importKey = url;
+      let importName = imports.get(importKey);
+
+      if (!importName) {
+        importName = `___CSS_LOADER_ICSS_IMPORT_${imports.size}___`;
+        imports.set(importKey, importName);
+
+        result.messages.push(
+          {
+            type: 'import',
+            value: {
+              importName,
+              url: options.urlHandler(url),
+              icss: true,
+              order: 0,
+              index,
+            },
+          },
+          {
+            type: 'api-import',
+            value: {
+              type: 'internal',
+              importName,
+              dedupe: true,
+              order: 0,
+              index,
+            },
+          }
+        );
+      }
+
+      for (const [replacementIndex, token] of Object.keys(tokens).entries()) {
+        const replacementName = `___CSS_LOADER_ICSS_IMPORT_${index}_REPLACEMENT_${replacementIndex}___`;
+        const localName = tokens[token];
+
+        importReplacements[token] = replacementName;
+
+        result.messages.push({
+          type: 'icss-replacement',
+          value: { replacementName, importName, localName },
+        });
+      }
+    }
 
     if (Object.keys(importReplacements).length > 0) {
       replaceSymbols(css, importReplacements);
     }
-
-    const { icssExports } = extractedICSS;
 
     for (const name of Object.keys(icssExports)) {
       const value = replaceValueSymbols(icssExports[name], importReplacements);
