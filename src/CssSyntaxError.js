3e8905973647396b125f30e91fed036854b60b2a
cap-Bernardito <cap-Bernardito@yandex.ru>
2020-09-23 17:41:22 +0300

    refactor: postcss error message

--- src/CssSyntaxError.js
+++ src/CssSyntaxError.js
@@ -2,7 +2,7 @@ export default class CssSyntaxError extends Error {
   constructor(error) {
     super(error);
 
-    const { reason, line, column } = error;
+    const { reason, line, column, file } = error;
 
     this.name = 'CssSyntaxError';
 
@@ -14,6 +14,7 @@ export default class CssSyntaxError extends Error {
       this.message += `(${line}:${column}) `;
     }
 
+    this.message += file ? `${file} ` : '<css input> ';
     this.message += `${reason}`;
 
     const code = error.showSourceCode();
