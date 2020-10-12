bc8496b4ac2066693ae9cbd7987779e0c3b84ec4
evilebottnawi <sheo13666q@gmail.com>
2020-10-12 20:27:26 +0300

    refactor: code

--- src/plugins/postcss-import-parser.js
+++ src/plugins/postcss-import-parser.js
@@ -7,6 +7,72 @@ import {
   requestify,
 } from '../utils';
 
+function visitor(result, parsedResults, node, key) {
+  // Convert only top-level @import
+  if (node.parent.type !== 'root') {
+    return;
+  }
+
+  // Nodes do not exists - `@import url('http://') :root {}`
+  if (node.nodes) {
+    result.warn(
+      "It looks like you didn't end your @import statement correctly. Child nodes are attached to it.",
+      { node }
+    );
+
+    return;
+  }
+
+  const { nodes: paramsNodes } = valueParser(node[key]);
+
+  // No nodes - `@import ;`
+  // Invalid type - `@import foo-bar;`
+  if (
+    paramsNodes.length === 0 ||
+    (paramsNodes[0].type !== 'string' && paramsNodes[0].type !== 'function')
+  ) {
+    result.warn(`Unable to find uri in "${node.toString()}"`, { node });
+
+    return;
+  }
+
+  let isStringValue;
+  let url;
+
+  if (paramsNodes[0].type === 'string') {
+    isStringValue = true;
+    url = paramsNodes[0].value;
+  } else {
+    // Invalid function - `@import nourl(test.css);`
+    if (paramsNodes[0].value.toLowerCase() !== 'url') {
+      result.warn(`Unable to find uri in "${node.toString()}"`, { node });
+
+      return;
+    }
+
+    isStringValue =
+      paramsNodes[0].nodes.length !== 0 &&
+      paramsNodes[0].nodes[0].type === 'string';
+    url = isStringValue
+      ? paramsNodes[0].nodes[0].value
+      : valueParser.stringify(paramsNodes[0].nodes);
+  }
+
+  // Empty url - `@import "";` or `@import url();`
+  if (url.trim().length === 0) {
+    result.warn(`Unable to find uri in "${node.toString()}"`, { node });
+
+    return;
+  }
+
+  parsedResults.push({
+    node,
+    url,
+    isStringValue,
+    mediaNodes: paramsNodes.slice(1),
+  });
+}
+
 const plugin = (options = {}) => {
   return {
     postcssPlugin: 'postcss-import-parser',
@@ -16,76 +82,7 @@ const plugin = (options = {}) => {
       return {
         AtRule: {
           import(atRule) {
-            // Convert only top-level @import
-            if (atRule.parent.type !== 'root') {
-              return;
-            }
-
-            // Nodes do not exists - `@import url('http://') :root {}`
-            if (atRule.nodes) {
-              result.warn(
-                "It looks like you didn't end your @import statement correctly. Child nodes are attached to it.",
-                { node: atRule }
-              );
-
-              return;
-            }
-
-            const { nodes: paramsNodes } = valueParser(atRule.params);
-
-            // No nodes - `@import ;`
-            // Invalid type - `@import foo-bar;`
-            if (
-              paramsNodes.length === 0 ||
-              (paramsNodes[0].type !== 'string' &&
-                paramsNodes[0].type !== 'function')
-            ) {
-              result.warn(`Unable to find uri in "${atRule.toString()}"`, {
-                node: atRule,
-              });
-
-              return;
-            }
-
-            let isStringValue;
-            let url;
-
-            if (paramsNodes[0].type === 'string') {
-              isStringValue = true;
-              url = paramsNodes[0].value;
-            } else {
-              // Invalid function - `@import nourl(test.css);`
-              if (paramsNodes[0].value.toLowerCase() !== 'url') {
-                result.warn(`Unable to find uri in "${atRule.toString()}"`, {
-                  node: atRule,
-                });
-
-                return;
-              }
-
-              isStringValue =
-                paramsNodes[0].nodes.length !== 0 &&
-                paramsNodes[0].nodes[0].type === 'string';
-              url = isStringValue
-                ? paramsNodes[0].nodes[0].value
-                : valueParser.stringify(paramsNodes[0].nodes);
-            }
-
-            // Empty url - `@import "";` or `@import url();`
-            if (url.trim().length === 0) {
-              result.warn(`Unable to find uri in "${atRule.toString()}"`, {
-                node: atRule,
-              });
-
-              return;
-            }
-
-            parsedResults.push({
-              atRule,
-              url,
-              isStringValue,
-              mediaNodes: paramsNodes.slice(1),
-            });
+            visitor(result, parsedResults, atRule, 'params');
           },
         },
         async OnceExit() {
@@ -97,7 +94,7 @@ const plugin = (options = {}) => {
           const tasks = [];
 
           for (const parsedResult of parsedResults) {
-            const { atRule, url, isStringValue, mediaNodes } = parsedResult;
+            const { node, url, isStringValue, mediaNodes } = parsedResult;
 
             let normalizedUrl = url;
             let prefix = '';
@@ -119,8 +116,8 @@ const plugin = (options = {}) => {
               // \
               // ';
               if (normalizedUrl.trim().length === 0) {
-                result.warn(`Unable to find uri in "${atRule.toString()}"`, {
-                  node: atRule,
+                result.warn(`Unable to find uri in "${node.toString()}"`, {
+                  node,
                 });
 
                 // eslint-disable-next-line no-continue
@@ -139,7 +136,7 @@ const plugin = (options = {}) => {
               continue;
             }
 
-            atRule.remove();
+            node.remove();
 
             if (isRequestable) {
               const request = requestify(normalizedUrl, options.rootContext);
