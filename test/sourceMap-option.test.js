c1d52ba60f3d4522c91844d8c1ad31008954354f
Alexander Akait <evilebottnawi@users.noreply.github.com>
2020-11-04 18:49:35 +0300

    test: fix

--- test/sourceMap-option.test.js
+++ test/sourceMap-option.test.js
@@ -501,8 +501,8 @@ describe('"sourceMap" option', () => {
 
       expect(chunkName).toBe(
         webpack.version[0] === '5'
-          ? 'main.7874af87d58bb3928914.bundle.js'
-          : 'main.535d928b9abb27709302.bundle.js'
+          ? 'main.12fc65e5279709bf97a4.bundle.js'
+          : 'main.0c7724aa71a6ab6d45fd.bundle.js'
       );
       expect(
         getModuleSource('fixtures/source-map/basic.css', stats)
