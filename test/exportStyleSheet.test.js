d1612c5242d67119d451fca1555bc39d279418be
evilebottnawi <sheo13666q@gmail.com>
2021-09-03 17:30:21 +0300

    test: more

--- test/exportStyleSheet.test.js
+++ test/exportStyleSheet.test.js
@@ -31,4 +31,52 @@ describe("exportStylesheet option", () => {
     expect(getWarnings(stats)).toMatchSnapshot("warnings");
     expect(getErrors(stats)).toMatchSnapshot("errors");
   });
+
+  it("should work with ECMA modules", async () => {
+    const compiler = getCompiler(
+      "./basic-import-assertion-css.js",
+      {
+        exportStyleSheet: true,
+        esModule: true,
+      },
+      {
+        target: "web",
+        output: {
+          path: path.resolve(__dirname, "./outputs"),
+          filename: "[name].bundle.js",
+          chunkFilename: "[name].chunk.js",
+          assetModuleFilename: "[name][ext]",
+        },
+      }
+    );
+    const stats = await compile(compiler);
+
+    expect(getModuleSource("./basic.css", stats)).toMatchSnapshot("module");
+    expect(getWarnings(stats)).toMatchSnapshot("warnings");
+    expect(getErrors(stats)).toMatchSnapshot("errors");
+  });
+
+  it("should work with CommonJS modules", async () => {
+    const compiler = getCompiler(
+      "./basic-import-assertion-css.js",
+      {
+        exportStyleSheet: true,
+        esModule: false,
+      },
+      {
+        target: "web",
+        output: {
+          path: path.resolve(__dirname, "./outputs"),
+          filename: "[name].bundle.js",
+          chunkFilename: "[name].chunk.js",
+          assetModuleFilename: "[name][ext]",
+        },
+      }
+    );
+    const stats = await compile(compiler);
+
+    expect(getModuleSource("./basic.css", stats)).toMatchSnapshot("module");
+    expect(getWarnings(stats)).toMatchSnapshot("warnings");
+    expect(getErrors(stats)).toMatchSnapshot("errors");
+  });
 });
